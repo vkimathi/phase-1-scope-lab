05a548175730e17822d3f5e9fcb0ef0d4ba0f21a
Victor Kimathi <vkimathi97@gmail.com>
2025-03-25 21:38:36 +0300

    scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+console.log(customerName);
+
+var bestCustomer;
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+console.log(bestCustomer);
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+console.log(bestCustomer);
+
+const leastFavoriteCustomer = 'ken';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'anton';
+}
+console.log(leastFavoriteCustomer);
